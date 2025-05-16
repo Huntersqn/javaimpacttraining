638a35010cb70d1194b281ffd377fa413e13473f
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-16 09:32:51 +0530

    Add files via upload

---
+++ even or odd of single integer.java
@@ -0,0 +1,29 @@
+even or odd of single integer
+
+import java.util.Scanner;
+
+public class SingleDigitChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a single-digit integer (0–9): ");
+        int num = scanner.nextInt();
+
+        if (num < 0 || num > 9) {
+            System.out.println("Invalid input! Please enter a single-digit integer.");
+        } else if (num == 0) {
+            System.out.println("zero");
+        } else if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+
+        scanner.close();
+    }
+}
+
+output:
+Enter a single-digit integer (0?9): 2
+even
+
+
---
+++ n is odd print weird and n is even.java
@@ -0,0 +1,31 @@
+n is odd print weird and n is even and inclusive range 2 to 5 print not weird and n is even the inclusive range 6 to 20 print weird and grater than 20 print not weird 
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else if (n > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
+
+output:
+input 3
+weird
+
+
+output:
