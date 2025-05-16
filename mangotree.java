3bfd704a89accfd804419a2d3b690c27927bf606
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-16 09:17:36 +0530

    Mangotree.java

--- mangotree.java
+++ mangotree.java
@@ -1,7 +1,11 @@
-//In the kingdom of Terebinthia, Leslie Burke is so much interested in gardening and hence she plants more trees in her garden. She plants trees in a rectangular fashion with the order of rows and columns and numbers the trees in a row-wise order. She planted the mango trees only in the 1st row, 1st column, and last column. So, given the tree number, your task is to find out whether the given tree is a mango tree or not. Now, write a program to check whether the given number denotes a mango tree or not.
-
+//In the kingdom of Terebinthia, Leslie Burke is so much interested in gardening and hence she plants more trees in her garden. 
+//She plants trees in a rectangular fashion with the order of rows and columns and numbers the trees in a row-wise order.
+//She planted the mango trees only in the 1st row, 1st column, and last column. 
+//So, given the tree number, your task is to find out whether the given tree is a mango tree or not.
+//Now, write a program to check whether the given number denotes a mango tree or not.
 //Input Format
-//Input consists of 3 integers. The first input denotes the number of rows. The second input denotes the number of columns. The third input denotes the tree number.
+//Input consists of 3 integers. The first input denotes the number of rows.
+//The second input denotes the number of columns. The third input denotes the tree number.
 
 import java.util.Scanner;
 public class MangoTreeChecker {
