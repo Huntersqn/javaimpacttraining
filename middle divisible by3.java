bf817eeaf898959ae173437aa46ce5c5e57d1bc4
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-16 09:35:43 +0530

    Add files via upload

---
+++ middle divisible by3.java
@@ -0,0 +1,27 @@
+ques: three digit number in which middle is divisible 3
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a 3 digit number: ");
+        int number = sc.nextInt();
+        
+        int digit2 = (number / 10) % 10;
+        
+        if (digit2 % 3 == 0) {
+            System.out.println("done");
+        } else {
+            System.out.println("notdone");
+        }
+    }
+}
+
+
+
+output:
+input 365
+done
+
