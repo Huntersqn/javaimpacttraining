2c73b227c7d2eba39b98345f29cc36b235774475
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-16 09:38:38 +0530

    Professor Kishore.java

--- Professor Kishore.java
+++ Professor Kishore.java
@@ -1,6 +1,8 @@
-ques:Professor Kishore wanted the kids to learn about Special numbers. (A two-digit number is said to be a special number if the sum of sum of its digits and the product of its digits is equal to the number itself. For example, 19 is a special number. The digits in 19 are 1 and 9. The sum of the digits is 10 and the product of the digits is 9. 10+9 = 19.)
-
-Can you help Kishore to write a program to find all special numbers between 2 limits m and n (both inclusive)? Assume that m and n are two-digit numbers.
+//Professor Kishore wanted the kids to learn about Special numbers.
+//(A two-digit number is said to be a special number if the sum of sum of its digits and 
+//the product of its digits is equal to the number itself. For example, 19 is a special number.
+//The digits in 19 are 1 and 9. The sum of the digits is 10 and the product of the digits is 9. 10+9 = 19.)
+//Can you help Kishore to write a program to find all special numbers between 2 limits m and n (both inclusive)? Assume that m and n are two-digit numbers.
 
 
 public class SpecialNumberFinder {
