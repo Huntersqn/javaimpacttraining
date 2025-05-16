7cdf6287e52b4b38eaa3d2b01d8d1561b44a3c0f
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-16 09:42:32 +0530

    Nikita and danny.java

--- Fibonacci series.java
+++ Fibonacci series.java
@@ -1,13 +1,13 @@
-Fibonacci series 
-
-Nikitha and Danny are close friends. They both are studying in the same school. Now they are on summer vacation. As they are bored, they ask their parents to take them to an exhibition. Nikitha and Danny play a game. In this game, there are three boxes with some number written on top. There is a treasure in one of the three boxes and it is present in the box with the second largest number on top. Also, to open the box, they need to decode the correct code of this box. The clue given to them to find the code is that it is the largest number that divides all three given numbers. So, now help Nikitha and Danny to decode the code.
-
-Input Format
-
-Input consists of three integers. The first input corresponds to the number of the first box. The second input corresponds to the number of the second box. The third input corresponds to the number of the third box.
+//Nikitha and Danny are close friends. They both are studying in the same school. Now they are on summer vacation.
+//As they are bored, they ask their parents to take them to an exhibition. Nikitha and Danny play a game.
+//In this game, there are three boxes with some number written on top. There is a treasure in one of the three boxes and 
+//it is present in the box with the second largest number on top. Also, to open the box, they need to decode the correct code of this box. 
+//The clue given to them to find the code is that it is the largest number that divides all three given numbers. So, now help Nikitha and Danny to decode the code.
+//Input Format
+//Input consists of three integers. The first input corresponds to the number of the first box.
+//The second input corresponds to the number of the second box. The third input corresponds to the number of the third box.
 
 import java.util.*;
-
 public class TreasureBoxGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
