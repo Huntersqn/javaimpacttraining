95283abecc0e92200e8b3e7a2e5849e224285a45
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-18 14:03:42 +0530

    leetcode169.java

--- leetcode169.java
+++ leetcode169.java
@@ -1,5 +1,5 @@
-leetcode169:Given an array nums of size n, return the majority element.
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//leetcode169:Given an array nums of size n, return the majority element.
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
 public class Solution {
     public int majorityElement(int[] nums) {
