ef9370c91d46815a595bb4ad577727c04214089b
Huntersqn <138161666+Huntersqn@users.noreply.github.com>
2025-05-21 09:02:43 +0530

    lee conducted a word game.java

--- lee conducted a word game.java
+++ lee conducted a word game.java
@@ -1,4 +1,7 @@
-lee conducted a word game  for his colleagues the game is everyone should  say a word that should not have any repeating characters in it.if a single character is repeated then the particular person can"t continue the game lee finds it difficult to disequalify the person from the gaem sincehe is not able to find the nonrespective characters for all the words help him find the winner of the game by writitng a program to find the first element which is non repertitive le that element must be present anywhetre else in the string in java simplified
+//lee conducted a word game  for his colleagues the game is everyone should  say a word that should not have any repeating characters in it.
+//if a single character is repeated then the particular person can"t continue the game lee finds it difficult to disequalify the person from the game since 
+//he is not able to find the nonrespective characters for all the words help him find the winner of the game by 
+//writitng a program to find the first element which is non repertitive le that element must be present anywhetre else in the string in java simplified
 
 public class Solution {
     public static char firstNonRepeatingChar(String word) {
